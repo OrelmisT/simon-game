61af9e08f6f4307bb8cc393f5161a8edf1f34156
OrelmisT <orelmistoribio@gmail.com>
2025-09-25 19:51:51 -0400

    Added fly deployment

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
-    store: new RedisStore({client: redisClient}),
+    store: new RedisStore({client: redisClient, prefix:'simon-game:'}),
     secret: config.SESSION_KEY,
     resave: false,
     rolling: true,
